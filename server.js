c406bcdf1f51b52f7e64380f8933d9b56c9af08d
biggiejr <jrmacej@gmail.com>
2017-09-19 12:49:48 +0200

    Update
    Testing purposes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World from a Restarted app built from a Docker image!\n');
+  res.send('Hello World from a Restarted app built from a Docker image! With a whole team but Richie is still the Bum\n');
 });
 
 app.listen(PORT, HOST);
