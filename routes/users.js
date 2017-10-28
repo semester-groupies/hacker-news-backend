2116dd40237d8bfa063eb5e81c8ca00969b023b2
Athinodoros Sgouromallis <athinodoros.sgouromallis@hotmail.com>
2017-10-28 14:35:54 +0200

    api user creation and fetch

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,10 @@ const bcrypt = require("bcryptjs");
 router.get('/', (req, res, next) => {
     res.json({name: 'John', surname: 'Williams'});
 });
-var neo4j = require('node-neo4j');
+var neo4j = require('neo4j-driver').v1;
+const driver = neo4j.driver("bolt://favl.dk:7687", neo4j.auth.basic("neo4j", "hackernes"));
 
-db = new neo4j('http://neo4j:hackernes@favl.dk:8474');
+var session = driver.session();
 var salt = bcrypt.genSaltSync(10);
 
 router.post('/create', (req, res, next) => {
@@ -24,19 +25,23 @@ router.post('/create', (req, res, next) => {
             res.send(msg);
         } else {
             // TODO: check if username is already in use
-            db.cypherQuery("CREATE (n:USER)", {
-                n: {
-                    username: user.username,
-                    password: bcrypt.hashSync(user.password, salt)
-                }
-            }, function (err, result) {
-                if (err) throw err;
-
-                console.log(result.data); // delivers an array of query results
-                console.log(result.columns); // delivers an array of names of objects getting returned
-                res.status(200);
-                res.end()
-            });
+            var userToCreate = {
+                username: user.username,
+                password: bcrypt.hashSync(user.password, salt)
+            };
+            session.run("CREATE (n:USER {userin} )", {userin: userToCreate})
+                .subscribe({
+                    onNext: function (record) {
+                    },
+                    onCompleted: function () {
+                        session.close();
+                        res.status(200).send(userToCreate);
+                    },
+                    onError: function (error) {
+                        console.log(error);
+                        res.send(error);
+                    }
+                });
 
         }
     }
@@ -47,6 +52,59 @@ router.post('/create', (req, res, next) => {
         res.send(msg);
     }
 });
+
+
+router.post('/login', (req, res, next) => {
+    const user = req.body;
+    var errors = "";
+    if (user) {
+        if (!user.hasOwnProperty('username'))
+            errors = errors + " , username is missing";
+        if (!user.hasOwnProperty('password'))
+            errors = errors + " , password is missing";
+        if (errors !== "") {
+            var msg = new Error();
+            msg.status = 400;
+            msg.message = "malformed body " + errors;
+            res.send(msg);
+        } else {
+            // TODO: check if username is already in use
+            var userToCreate = {
+                username: user.username,
+                password: bcrypt.hashSync(user.password, salt)
+            };
+            session.run("Match (n:USER)" +
+                "where n.username = {username} " +
+                "  return n", {username: userToCreate.username})
+                .subscribe({
+                    onNext: function (record) {
+                        console.log(record);
+                        if(bcrypt.compareSync(user.password,record._fields[0].properties.password)){
+                            res.status(200).send(record._fields[0]);
+                        }else{
+                            res.status(401).send("Wrong Username or Password")
+                        }
+                    },
+                    onCompleted: function () {
+                        session.close();
+                        res.status(200).send(userToCreate);
+                    },
+                    onError: function (error) {
+                        // console.log(error);
+                        res.send(error);
+                    }
+                });
+
+        }
+    }
+    else {
+        var msg = new Error();
+        msg.status = 400;
+        msg.message = "Empty body";
+        res.send(msg);
+    }
+});
+
 router.post('/user', (req, res, next) => {
     const user = req.body;
 
