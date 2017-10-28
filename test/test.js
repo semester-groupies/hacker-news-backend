6b07f2111c93833155ed0d2ce54a917252838960
favl <favl.dk@gmail.com>
2017-10-28 11:39:10 +0200

    test fix

--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
-var supertest = require("supertest");
+var app = require("./../server");
+var request = require("supertest")(app);
 var should = require("should");
 
 // This agent refers to PORT where the program is running.
 
-var server = supertest.agent("http://localhost:8080");
+// var server = supertest.agent("http://localhost:8080");
 
 // UNIT test begin
 
@@ -12,16 +13,15 @@ describe("SAMPLE unit test",function(){
   // #1 should return home page
   it("should return home page",function(done){
     // calling home page
-    server
-    .get("/")
-    .expect("Content-type","application/json; charset=utf-8")
-    .expect(200) // THis is HTTP response
-    .end(function(err,res){
-      // HTTP status should be 200
-        console.log(err)
-      res.status.should.equal(200);
-      done();
-    });
+    request
+        .get("/")
+        .expect("Content-type","application/json; charset=utf-8")
+        .expect(200) // THis is HTTP response
+        .end(function(err,res){
+          // HTTP status should be 200
+          res.status.should.equal(200);
+          done();
+        });
   });
 
 });
